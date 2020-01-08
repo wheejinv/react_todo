c0d1c73e16977dd9c936f2779eb791622485a318
whee <wheejinv@naver.com>
2020-01-09 08:02:30 +0900

    챕터2. JSX 기본 문법

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const text = `당신은 어썸한가욤?`;
+	const condition = true;
+	const style = {
+		backgroundColor: "gray", // background-color
+		border: "1px solid black",
+		height: Math.round(Math.random() * 300) + 50,
+		width: Math.round(Math.random() * 300) + 50,
+		WebkitTransition: "all", // 웹 브라우저 접두어인 -webkit
+		MozTransition: "all", // 웹 브라우저 접두어인 -mos
+		msTransition: "all", // 하지만 예외로 대문자로 바꾸지 않고 소문자 ms 로 작성.
+	};
+
+	return (
+		<div className="my-div">
+			{ /* 요소 박에서의 주석은 이렇게 작성함. */}
+			<h1>리액트 안녕</h1>
+			<h2>{text}</h2>
+			{
+				condition && "참입니다"
+			}
+			<div
+				style={style}
+				// self-closed 태그에서만 작동하는 주석이라고 합니다.
+			/>
+			// 이건 그냥 렌더링 됨 ㅋ
+		</div>
+	);
 }
 
 export default App;
