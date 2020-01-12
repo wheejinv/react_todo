39f670cb6bd97337af27793d512603cb28fe4a8c
whee <wheejinv@naver.com>
2020-01-13 02:28:56 +0900

    챕터5. ref: DOM에 이름달기 - 를 하지 않고 state로 구현할 수 있는 기능.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import EventPractice from "./component/EventPractice";
+import ValidationSample from "./component/ValidationSample";
 
 function App() {
 	return (
 		// <MyComponent name={"whee"}/>
-		<EventPractice />
+		// <EventPractice />
+		<ValidationSample />
 	);
 }
 
