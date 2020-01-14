f0cdb7cfe365183f8d4089e2be1b1ec08cab5f9b
whee <wheejinv@naver.com>
2020-01-15 01:51:10 +0900

    챕터6. state에 초기 데이터 담고, 데이터 추가 기능 구현

--- src/component/IterationSample.js
+++ src/component/IterationSample.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
 
 class IterationSample extends Component {
+
+	state = {
+		names: ["눈사람", "얼음", "눈", "바람"],
+		name: "",
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			name: e.target.value,
+		});
+	};
+
+	handleInsert = () => {
+		this.setState({
+			names: this.state.names.concat(this.state.name),
+			name: "",
+		});
+	};
+
 	render() {
-		const names = ["눈사람", "얼음", "눈", "바람"];
-		const nameList = names.map((name, index) => {
+		const nameList = this.state.names.map((name, index) => {
 			return (
 				<li key={index}>
 					{name}
@@ -13,6 +31,10 @@ class IterationSample extends Component {
 
 		return (
 			<div>
+				<input
+					onChange={this.handleChange}
+					value={this.state.name}/>
+				<button onClick={this.handleInsert}>추가</button>
 				<ul>
 					{nameList}
 				</ul>
