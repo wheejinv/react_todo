e480849a3086d8e0eedf58940e29c5af05fc2c71
whee <wheejinv@naver.com>
2020-01-17 03:16:08 +0900

    챕터7. 컨포넌트의 라이프사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
-import IterationSample from "./component/IterationSample";
+import LifeCycleSample from "./component/LifeCycleSample";
+
+// 랜덤 색상을 생성
+function getRandomColor() {
+	return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+	state = {
+		color: '#0000000',
+	};
+
+	handleClick = () => {
+		this.setState({
+			color: getRandomColor(),
+		});
+	};
+
 	render() {
 		return (
-			<IterationSample/>
+			<div>
+				<button onClick={this.handleClick}>랜덤 색상</button>
+				<LifeCycleSample color={this.state.color}/>
+			</div>
 		);
 	}
 }
