89b4069575154c03bd2dcfd2ec340d09a6b46556
whee <wheejinv@naver.com>
2020-01-19 19:11:51 +0900

    챕터9. classNames 의 bind 기능 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import classNames from 'classnames'
+import classNames from 'classnames/bind';
 import styles from "./App.module.css";
+
+const cx = classNames.bind(styles);
 
 class App extends Component {
 	render() {
@@ -8,7 +10,8 @@ class App extends Component {
 			// 클래스가 여러개 일 때 사용하는 방법
 			// <div className={[styles.box, styles.blue].join(' ')}>
 			// classNames 라이브러리를 이용하는 방
-			<div className={classNames(styles.box, styles.blue)}>
+			// <div className={classNames(styles.box, styles.blue)}>
+			<div className={cx('box', 'blue')}>
 
 			</div>
 		);
