7d32c59c9be0208c5c09531d1e3aeff111907968
whee <wheejinv@naver.com>
2020-01-19 19:08:38 +0900

    챕터9. 클래스가 여러개 일 때 사용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import classNames from 'classnames'
 import styles from "./App.module.css";
 
 class App extends Component {
 	render() {
 		return (
-			<div className={styles.box}>
+			// 클래스가 여러개 일 때 사용하는 방법
+			// <div className={[styles.box, styles.blue].join(' ')}>
+			// classNames 라이브러리를 이용하는 방
+			<div className={classNames(styles.box, styles.blue)}>
 
 			</div>
 		);
